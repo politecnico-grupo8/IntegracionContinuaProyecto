9d6693281c741c9e03af0cb9de71f49e6a8f8be4
Andres Sierra <andres_sebastian2009@hotmail.com>
2020-11-08 14:12:13 -0500

    Se agrega el componente al app routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,12 @@ import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule  } from "@angular/router";
 
 import { PeopleComponent } from './components/people/people.component';
-
+import { PlanetsComponent } from './components/planets/planets.component';
 
 const appRoutes: Routes = [
     {path: '', component: PeopleComponent},
-    {path: 'people', component: PeopleComponent}
+    {path: 'people', component: PeopleComponent},
+    {path: 'planets', component: PlanetsComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
