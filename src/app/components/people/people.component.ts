01f0235167d1b2167a0c78aa29c6f2f93513bfae
alexis7 <baironnaranjo@gmail.com>
2020-11-08 12:13:58 -0500

    Se corrige error temporal al tratar de consumir el servicio people que no esta funcionando correctamente

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { People } from '../../models/people';
-import { PeopleService } from '../../services/people.service';
+
 
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
-  styleUrls: ['./people.component.css'],
-  providers: [PeopleService]
+  styleUrls: ['./people.component.css']
 })
 export class PeopleComponent implements OnInit {
 
@@ -15,26 +14,18 @@ export class PeopleComponent implements OnInit {
   public result;
 
   constructor(
-    private _peolpleService: PeopleService
+
   ) {
     this.page_title = 'People';
     this.people = new People('','');
    }
 
   ngOnInit() {
-    
+
   }
 
   onSubmit() {
-    this._peolpleService.getPeople(this.people).subscribe(
-      response => {
-        this.result = response.results;
-        console.log(this.result);
-      },
-      error =>{
-        console.log(error);
-      }
-    );
+
   }
 
 }
