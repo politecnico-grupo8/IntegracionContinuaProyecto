d970ee991f840f18e7feb4c87bb5a63110401178
Danielrpelaez <72061176+Danielrpelaez@users.noreply.github.com>
2020-11-24 08:53:33 -0500

    Agregar componente

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,11 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('star-wars');
   });
 
-
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to star-wars!');
   });
 });
+
